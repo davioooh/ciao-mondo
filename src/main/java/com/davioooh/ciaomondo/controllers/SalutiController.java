af6995a9ca11f3f7d87298f35790426ede4e1306
davioooh <davidcastelletti@yahoo.it>
2017-09-04 10:33:38 +0200

    Aggiunti nuovi handler e mapping a SalutiController

--- src/main/java/com/davioooh/ciaomondo/controllers/SalutiController.java
+++ src/main/java/com/davioooh/ciaomondo/controllers/SalutiController.java
@@ -1,13 +1,26 @@
 package com.davioooh.ciaomondo.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping("/")
 public class SalutiController {
 
-    @RequestMapping("/")
-    public String unSaluto(){
+    @RequestMapping(method = RequestMethod.GET)
+    public String unSaluto() {
         return "ciao-mondo";
+    }
+
+    @GetMapping(params = "mio-parametro")
+    public String unSalutoConParametro() {
+        return "ciao-mondo-param";
+    }
+
+    @GetMapping(headers = "mio-header=test")
+    public String unSalutoConHeader() {
+        return "ciao-mondo-header";
     }
 }
