b139a57a05dd1bc27b409016b1b31d0fd58e7ade
davioooh <davidcastelletti@yahoo.it>
2017-09-29 10:46:47 +0200

    Aggiunto parametro e header al metodo `unSaluto`

--- src/main/java/com/davioooh/ciaomondo/controllers/SalutiController.java
+++ src/main/java/com/davioooh/ciaomondo/controllers/SalutiController.java
@@ -2,17 +2,21 @@ package com.davioooh.ciaomondo.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/")
 public class SalutiController {
 
     @GetMapping
-    public String unSaluto(Model model) {
-        model.addAttribute("nome", "David");
+    public String unSaluto(
+            @RequestParam(name = "nome", defaultValue = "Tizio") String nome
+            , @RequestHeader("User-Agent") String userAgent
+            , Model model) {
+        model.addAttribute("nome", nome);
+        model.addAttribute("userAgent", userAgent);
         return "ciao-mondo";
     }
 
